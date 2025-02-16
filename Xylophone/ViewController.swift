781bd173c5509eec71003f1258fb4af59f361265
drew0ps <ad.marton@proton.me>
2025-02-16 23:30:51 +0100

    Progress on app

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,15 +7,50 @@
 //
 
 import UIKit
+import AVFoundation
+var player: AVAudioPlayer!
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
-
     
+    func playSound(soundName: String) {
+        let url = Bundle.main.url(forResource: soundName, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+    }
+
+
+
+    @IBAction func keyPressedC(_ sender: UIButton) {
+        playSound(soundName: "C")
+    }
+    
+    @IBAction func keyPressedD(_ sender: UIButton) {
+        playSound(soundName: "D")
+    }
+    
+    @IBAction func keyPressedE(_ sender: UIButton) {
+        playSound(soundName: "E")
+    }
+    
+    @IBAction func keyPressedF(_ sender: UIButton) {
+        playSound(soundName: "F")
+    }
+    
+    @IBAction func keyPressedG(_ sender: UIButton) {
+        playSound(soundName: "G")
+    }
+    
+    @IBAction func keyPressedA(_ sender: UIButton) {
+        playSound(soundName: "A")
+    }
+    
+    @IBAction func keyPressedB(_ sender: UIButton) {
+        playSound(soundName: "B")
+    }
 
 }
 
